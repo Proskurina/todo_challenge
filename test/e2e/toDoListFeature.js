f2cc6e3e78f644c454db0906ae1ffdd2bacc7d89
Proskurina <yproskurina@yahoo.com>
2015-10-25 22:32:24 +0000

    One test added

--- test/e2e/toDoListFeature.js
+++ test/e2e/toDoListFeature.js
@@ -1,6 +1,21 @@
 describe('ToDo List', function() {
-  it('has a title', function() {
+
+  var searchBox = element(by.model('listCtrl.task.entry'))
+  var searchButton = element(by.className('btn'))
+
+  beforeEach(function() {
     browser.get('http://localhost:3000');
+  })
+
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual('ToDo List');
   });
+
+  it('can enter a task', function() {
+    searchBox.sendKeys('write a test');
+    searchButton.click();
+    expect(element(by.className('ng-binding')).getText()).toContain('write a test active');
+  });
+
+
 });
